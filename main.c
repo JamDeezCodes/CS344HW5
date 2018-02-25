e3f11cb65719b2eb85d2de2d3423f7d555071e44
ToBeAmped <martinja@oregonstate.edu>
2018-02-25 00:42:37 -0800

    Read in file to buffer and print to stdout

--- main.c
+++ main.c
@@ -18,67 +18,101 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	//Store file in buffer
-	
-	FILE *imageToProcess;
-	imageToProcess = fopen(argv[2], "r");
-	
 	int ***pixels;
 	char type[70];;
-	int width, height, maxPixelValue;
+	int width, height, max_pixel_value;
 	int red, blue, green;
+	int i, j;
 	
-	if(!imageToProcess)
+	//Store file in buffer
+	
+	FILE *image_to_process;
+	image_to_process = fopen(argv[2], "r");
+	
+	if(!image_to_process)
 	{
 		fprintf(stderr, "This CANNAWT Beeee\n");
 		exit(1);
 	}
 	else
 	{
-		//Store file header
-		fscanf(imageToProcess, "%s", type);
-		fscanf(imageToProcess, "%d", &width);
-		fscanf(imageToProcess, "%d", &height);
-		fscanf(imageToProcess, "%d", &maxPixelValue);
-		fclose(imageToProcess);
-		fprintf(stderr, "type: %s width: %d height: %d maxPixelValue: %d\n", type, width, height, maxPixelValue);
+		// Store file header
+		fscanf(image_to_process, "%s", type);
+		fscanf(image_to_process, "%d", &width);
+		fscanf(image_to_process, "%d", &height);
+		fscanf(image_to_process, "%d", &max_pixel_value);
+		
+		// Allocate array for pixels
+		pixels = (int ***) malloc(sizeof(int **) * height);
+		for(i = 0; i < height; i++)
+		{
+			pixels[i] = (int **) malloc(sizeof(int *) * width);
+			for(j = 0; j < width; j++)
+			{
+				pixels[i][j] = (int *) malloc(sizeof(int) * 3);
+			}
+		}
+		
+		for(i = 0; i < height; i++)
+		{
+			for(j = 0; j < width; j++)
+			{
+				fscanf(image_to_process, "%d", &red);
+				fscanf(image_to_process, "%d", &blue);
+				fscanf(image_to_process, "%d", &green);
+				pixels[i][j][0] = red;
+				pixels[i][j][1] = blue;
+				pixels[i][j][2] = green;
+			}
+		}
+		
+		//Perform image processing
+		
+		// int n = atoi(argv[1]);
+		// int i;
+		// pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t)*n);
+		
+		// for(i = 0; i < n; i++)
+		// {
+			// printf("main: spawning thread %d..\n", i);
+			// pthread_create(&threads[i], NULL, fx, (void *)(i + 1));
+		// }
+
+		// for(i = 0; i < n; i++)
+		// {
+			// printf("main: joining thread %d..\n", i);
+			// pthread_join(threads[i], NULL);
+		// }
+		
+		//Print image to stdout
+		
+		printf("%s\n", type);
+		printf("%d ", width);
+		printf("%d\n", height);
+		printf("%d\n", max_pixel_value);
+		
+		
+		for(i = 0; i < height; i++)
+		{
+			for(j = 0; j < width; j++)
+			{
+				printf("%d %d %d\n", pixels[i][j][0], pixels[i][j][1], pixels[i][j][2]);
+			}
+		}
+		
+		for(i = 0; i < height; i++)
+		{
+			for(j = 0; j < width; j++)
+			{
+				free(pixels[i][j]);
+			}
+			free(pixels[i]);
+		}
+		
+		free(pixels);
+		
+		fclose(image_to_process);
 		
 	}
 	
-
-	
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	// int n = atoi(argv[1]);
-	// int i;
-	// pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t)*n);
-	
-	// for(i = 0; i < n; i++)
-	// {
-		// printf("main: spawning thread %d..\n", i);
-		// pthread_create(&threads[i], NULL, fx, (void *)(i + 1));
-	// }
-
-	// for(i = 0; i < n; i++)
-	// {
-		// printf("main: joining thread %d..\n", i);
-		// pthread_join(threads[i], NULL);
-	// }
 }
