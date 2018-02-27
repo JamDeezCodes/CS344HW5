71c7b8e60f32658bd9ff9c7876d13ba4f62abffb
ToBeAmped <martinja@oregonstate.edu>
2018-02-27 12:49:53 -0800

    Allocate new pixel array outside of left and right rotate methods

--- main.c
+++ main.c
@@ -8,8 +8,11 @@ typedef struct image
 	int rows;
 	int columns;
 	int max_pixel_value;
+	int row_to_end_at;
 	float contrast_percentage;
 } image;
+
+int ***pixels;
 
 void *change_image_to_red (void *arg)
 {
@@ -133,21 +136,7 @@ void *right_rotate_image (void *arg)
 	
 	int row_index, column_index;
 	int red_temp, blue_temp, green_temp;
-	int i, j, ***pixels;
-	
-	int new_rows = image_object->columns;
-	int new_columns = image_object->rows;
-	
-	// Allocate new array for pixels
-	pixels = (int ***) malloc(sizeof(int **) * image_object->columns);
-	for(i = 0; i < image_object->columns; i++)
-	{
-		pixels[i] = (int **) malloc(sizeof(int *) * image_object->rows);
-		for(j = 0; j < image_object->rows; j++)
-		{
-			pixels[i][j] = (int *) malloc(sizeof(int) * 3);
-		}
-	}
+	int i, j;
 	
 	for
 	(	
@@ -171,23 +160,6 @@ void *right_rotate_image (void *arg)
 		}
 	}
 	
-	// Free old 3D pixel array
-	
-	for(i = 0; i < image_object->rows; i++)
-	{
-		for(j = 0; j < image_object->columns; j++)
-		{
-			free(image_object->pixels[i][j]);
-		}
-		free(image_object->pixels[i]);
-	}
-	
-	free(image_object->pixels);
-	
-	image_object->pixels = pixels;
-	image_object->rows = new_rows;
-	image_object->columns = new_columns;
-	
 	return NULL;
 }
 
@@ -197,21 +169,7 @@ void *left_rotate_image (void *arg)
 	
 	int row_index, column_index;
 	int red_temp, blue_temp, green_temp;
-	int i, j, ***pixels;
-	
-	int new_rows = image_object->columns;
-	int new_columns = image_object->rows;
-	
-	// Allocate new array for pixels
-	pixels = (int ***) malloc(sizeof(int **) * image_object->columns);
-	for(i = 0; i < image_object->columns; i++)
-	{
-		pixels[i] = (int **) malloc(sizeof(int *) * image_object->rows);
-		for(j = 0; j < image_object->rows; j++)
-		{
-			pixels[i][j] = (int *) malloc(sizeof(int) * 3);
-		}
-	}
+	int i, j;
 	
 	for
 	(	
@@ -234,22 +192,6 @@ void *left_rotate_image (void *arg)
 			
 		}
 	}
-	
-	//Free old 3D pixel array
-	for(i = 0; i < image_object->rows; i++)
-	{
-		for(j = 0; j < image_object->columns; j++)
-		{
-			free(image_object->pixels[i][j]);
-		}
-		free(image_object->pixels[i]);
-	}
-	
-	free(image_object->pixels);
-	
-	image_object->pixels = pixels;
-	image_object->rows = new_rows;
-	image_object->columns = new_columns;
 	
 	return NULL;
 }
@@ -306,32 +248,32 @@ int main(int argc, char **argv)
 
 	// int ***pixels;
 	char type[70];;
-	int width, height, max_pixel_value;
+	int columns, rows, max_pixel_value;
 	int red, blue, green;
 	int i, j;
 	struct image *image_object = (struct image*) malloc(sizeof(struct image*));
 	
 	// Read file header from stdin
 	fscanf(stdin, "%s", type);
-	fscanf(stdin, "%d", &width);
-	fscanf(stdin, "%d", &height);
+	fscanf(stdin, "%d", &columns);
+	fscanf(stdin, "%d", &rows);
 	fscanf(stdin, "%d", &max_pixel_value);
 	
 	// Allocate array for pixels
-	image_object->pixels = (int ***) malloc(sizeof(int **) * height);
-	for(i = 0; i < height; i++)
+	image_object->pixels = (int ***) malloc(sizeof(int **) * rows);
+	for(i = 0; i < rows; i++)
 	{
-		image_object->pixels[i] = (int **) malloc(sizeof(int *) * width);
-		for(j = 0; j < width; j++)
+		image_object->pixels[i] = (int **) malloc(sizeof(int *) * columns);
+		for(j = 0; j < columns; j++)
 		{
 			image_object->pixels[i][j] = (int *) malloc(sizeof(int) * 3);
 		}
 	}
 	
 	// Store pixel RGB values inside 3D array
-	for(i = 0; i < height; i++)
+	for(i = 0; i < rows; i++)
 	{
-		for(j = 0; j < width; j++)
+		for(j = 0; j < columns; j++)
 		{
 			fscanf(stdin, "%d", &red);
 			fscanf(stdin, "%d", &blue);
@@ -343,14 +285,15 @@ int main(int argc, char **argv)
 	}
 	
 	// Perform image processing
-	
-	image_object->rows = height;
-	image_object->columns = width;
+	image_object->rows = rows;
+	image_object->columns = columns;
 	image_object->max_pixel_value = max_pixel_value;	
 	
 	int number_of_threads = atoi(argv[1]);
 	
-	//TODO: split rows up according to number_of_threads count
+	//TODO: split rows up according to number_of_threads count, last thread gets most responsibility
+	int rows_per_thread = image_object->rows/number_of_threads;
+	int row_to_stop_at = 0;
 	
 	// Handle different kinds of image transformations
 	if(strcmp(argv[2], "-red") == 0)
@@ -404,11 +347,24 @@ int main(int argc, char **argv)
 	}
 	else if(strcmp(argv[2], "-L") == 0)
 	{
+		int new_rows = image_object->columns;
+		int new_columns = image_object->rows;
+		
+		// Allocate new array for pixels
+		pixels = (int ***) malloc(sizeof(int **) * image_object->columns);
+		for(i = 0; i < image_object->columns; i++)
+		{
+			pixels[i] = (int **) malloc(sizeof(int *) * image_object->rows);
+			for(j = 0; j < image_object->rows; j++)
+			{
+				pixels[i][j] = (int *) malloc(sizeof(int) * 3);
+			}
+		}
+		
 		pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t)*number_of_threads);
 		
 		for(i = 0; i < number_of_threads; i++)
 		{
-
 			pthread_create(&threads[i], NULL, left_rotate_image, (void *)(image_object));
 		}
 
@@ -417,9 +373,40 @@ int main(int argc, char **argv)
 
 			pthread_join(threads[i], NULL);
 		}
+		
+		//Free old 3D pixel array
+		for(i = 0; i < image_object->rows; i++)
+		{
+			for(j = 0; j < image_object->columns; j++)
+			{
+				free(image_object->pixels[i][j]);
+			}
+			free(image_object->pixels[i]);
+		}
+		
+		free(image_object->pixels);
+		
+		image_object->pixels = pixels;
+		image_object->rows = new_rows;
+		image_object->columns = new_columns;
 	}
 	else if(strcmp(argv[2], "-R") == 0)
 	{
+		//allocate new pixel array
+		int new_rows = image_object->columns;
+		int new_columns = image_object->rows;
+		
+		// Allocate new array for pixels
+		pixels = (int ***) malloc(sizeof(int **) * image_object->columns);
+		for(i = 0; i < image_object->columns; i++)
+		{
+			pixels[i] = (int **) malloc(sizeof(int *) * image_object->rows);
+			for(j = 0; j < image_object->rows; j++)
+			{
+				pixels[i][j] = (int *) malloc(sizeof(int) * 3);
+			}
+		}
+		
 		pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t)*number_of_threads);
 		
 		for(i = 0; i < number_of_threads; i++)
@@ -433,6 +420,22 @@ int main(int argc, char **argv)
 
 			pthread_join(threads[i], NULL);
 		}
+		
+		//Free old 3D pixel array
+		for(i = 0; i < image_object->rows; i++)
+		{
+			for(j = 0; j < image_object->columns; j++)
+			{
+				free(image_object->pixels[i][j]);
+			}
+			free(image_object->pixels[i]);
+		}
+		
+		free(image_object->pixels);
+		
+		image_object->pixels = pixels;
+		image_object->rows = new_rows;
+		image_object->columns = new_columns;
 	}
 	else if(strcmp(argv[2], "-I") == 0)
 	{
@@ -473,8 +476,8 @@ int main(int argc, char **argv)
 	
 	//Print image to stdout
 	printf("%s\n", type);
-	printf("%d ", width);
-	printf("%d\n", height);
+	printf("%d ", columns);
+	printf("%d\n", rows);
 	printf("%d\n", max_pixel_value);
 	
 	
@@ -486,9 +489,9 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	for(i = 0; i < height; i++)
+	for(i = 0; i < rows; i++)
 	{
-		for(j = 0; j < width; j++)
+		for(j = 0; j < columns; j++)
 		{
 			free(image_object->pixels[i][j]);
 		}
